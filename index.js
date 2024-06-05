07286a8613c8e1eeb4cd9921847c5165721f63b4
Mason Barnes <barnesmason980@gmail.com>
2024-06-05 11:01:45 -0700

    lab comeplete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Greg',
+    streetAddress: '1 Pine',
+
+
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    let employeeClone = {...obj, }
+    
+    employeeClone[key] = value;
+    
+    return employeeClone
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+
+    obj[key] = value;
+    return obj;
+
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee[key];
+    return employee;
+}
+
+
